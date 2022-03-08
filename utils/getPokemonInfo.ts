d22c80a44349f1473fe82c8ad5b7e5e9c41a3a02
stargazing4122 <jadenyeiden@gmail.com>
2022-03-08 15:20:08 -0500

    seccion 6

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,13 +2,17 @@ import { pokeApi } from "../api";
 import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async(idOrName: string ) => {
-  const { data } = await  pokeApi.get<Pokemon>(`pokemon/${idOrName}`);
-  return {
-    id: data.id,
-    name: data.name,
-    weight: data.weight,
-    height: data.height,
-    abilities: data.abilities,
-    sprites: data.sprites,
+  try {
+    const { data } = await  pokeApi.get<Pokemon>(`pokemon/${idOrName}`);
+    return {
+      id: data.id,
+      name: data.name,
+      weight: data.weight,
+      height: data.height,
+      abilities: data.abilities,
+      sprites: data.sprites,
+    }
+  } catch (err) {
+    return null;
   }
 }
